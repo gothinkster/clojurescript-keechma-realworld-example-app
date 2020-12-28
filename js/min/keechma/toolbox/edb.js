89e300e4761f4239b0f887d262eebedcefda4112
Mihael Konjevic <konjevic@gmail.com>
2020-12-28 14:49:16 +0100

    Update with keechma/next app

--- js/min/keechma/toolbox/edb.js
+++
@@ -1,4 +0,0 @@
-// Compiled by ClojureScript 1.9.854 {:static-fns true, :optimize-constants true, :elide-asserts true}
-goog.provide('keechma.toolbox.edb');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
--- js/min/realworld/settings.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.9.854 {:static-fns true, :optimize-constants true, :elide-asserts true}
-goog.provide('realworld.settings');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-realworld.settings.api_endpoint = "https://conduit.productionready.io/api";
-realworld.settings.articles_per_page = (10);
-realworld.settings.jwt_local_storage_name = "conduit-jwt-token";
